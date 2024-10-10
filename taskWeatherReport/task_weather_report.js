e33a9c0a626a227c7ccc61a2524d3b5c73be1021
kevinaso <kevin_david_saldana@hotmail.com>
2024-10-10 16:05:48 -0500

    weather report by longitudr latitude task solved

---
+++ taskWeatherReport/task_weather_report.js
@@ -0,0 +1,26 @@
+function showweatherDetails(event) {
+    event.preventDefault();
+
+    const latitude = document.getElementById('latitude').value;
+    const longitude = document.getElementById('longitude').value;
+    const apiKey = 'db8098ef85ee599c1ab2435e2cc246a5'; 
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    fetch(apiUrl)
+        .then(response => 
+            response.json()
+        )
+        .then(data => {
+            console.log("data es "+data)
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                                <p>Temperature: ${data.main.temp} &#8451;</p>
+                                <p>Weather: ${data.weather[0].description}</p>`;
+        })
+        .catch(error => {
+            console.error('Error fetching weather:', error);
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+          });
+        
+}
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
